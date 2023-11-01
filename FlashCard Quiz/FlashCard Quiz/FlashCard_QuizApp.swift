6e10c2ea68d2e87ef6123569b07665346f5fb9e4
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-01 13:58:50 +0500

    Initial Commit

---
+++ FlashCard Quiz/FlashCard Quiz/FlashCard_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCard_QuizApp.swift
+//  FlashCard Quiz
+//
+//  Created by apple on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashCard_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
