903c5ff68c824eb9b607d25d02b2fa07f564d939
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-01 23:27:39 +0500

    created FlashView for navigation btw 2 screen

---
+++ FlashCard Quiz/FlashCard Quiz/Views/FlashView.swift
@@ -0,0 +1,20 @@
+//
+//  FlashView.swift
+//  FlashCard Quiz
+//
+//  Created by apple on 01/11/2023.
+//
+
+import SwiftUI
+
+struct FlashView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+struct FlashView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlashView()
+    }
+}
