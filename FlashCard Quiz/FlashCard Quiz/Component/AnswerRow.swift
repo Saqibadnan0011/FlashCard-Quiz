890a87650f2c6b8e937a2d089483b6f4849ee223
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-01 18:13:36 +0500

    Add feature is AnswerRow

---
+++ FlashCard Quiz/FlashCard Quiz/Component/AnswerRow.swift
@@ -0,0 +1,50 @@
+//
+//  AnswerRow.swift
+//  FlashCard Quiz
+//
+//  Created by apple on 01/11/2023.
+//
+
+import SwiftUI
+
+struct AnswerRow: View {
+    
+    let answer: Answer
+    @State private var isSelected = false
+    
+    var green = Color(hue: 0.437, saturation: 0.711, brightness: 0.711)
+                      
+    var red = Color(red: 0.71, green: 0.094, blue: 0.1)
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(systemName: "circle.fill")
+                .font(.caption)
+            
+            Text(answer.text)
+                .bold()
+            
+            if isSelected {
+                Spacer()
+                
+                Image(systemName: answer.isCorrect ? "checkmark.circle.fill" : "x.circle.fill")
+                    .foregroundColor(answer.isCorrect ? green : red)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundColor(Color("AccentColor"))
+        .background(.white)
+        .cornerRadius(10)
+        .shadow(color: .gray, radius: 5, x: 0.5, y: 0.5)
+        .onTapGesture {
+            isSelected = true
+        }
+    }
+}
+
+struct AnswerRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerRow(answer: Answer(text: "Single", isCorrect: false))
+    }
+}
