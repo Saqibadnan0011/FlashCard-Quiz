f2a4e3ec3f2c691aa0e8bf0d0865dfad558e636f
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-01 17:02:27 +0500

    Change button properties

---
+++ FlashCard Quiz/FlashCard Quiz/Component/LetGoButton.swift
@@ -0,0 +1,28 @@
+//
+//  LetGoButton.swift
+//  FlashCard Quiz
+//
+//  Created by apple on 01/11/2023.
+//
+
+import SwiftUI
+
+struct LetGoButton: View {
+    let text: String
+    let background: Color = Color("AccentColor")
+    var body: some View {
+        Text(text)
+            .foregroundColor(.black)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+    }
+}
+
+struct LetGoButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LetGoButton(text: "Next")
+    }
+}
