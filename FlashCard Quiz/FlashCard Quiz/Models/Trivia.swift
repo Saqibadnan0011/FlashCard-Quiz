800b4b72068f9b2d0d1af831d9ca76e4c92f5d34
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-02 02:26:22 +0500

    Add Api functionalities

---
+++ FlashCard Quiz/FlashCard Quiz/Models/Trivia.swift
@@ -0,0 +1,47 @@
+//
+//  Flash.swift
+//  FlashCard Quiz
+//
+//  Created by apple on 01/11/2023.
+//
+
+import Foundation
+
+struct Trivia: Decodable {
+    var results: [Result]
+    
+    struct Result: Decodable, Identifiable {
+        var id: UUID {
+            UUID()
+        }
+        var category: String
+        var type: String
+        var difficulty: String
+        var question: String
+        var correctAnswer: String
+        var incorrectAnswers: [String]
+        
+        var formattedQuestion: AttributedString {
+            do {
+                return try AttributedString(markdown: question)
+            } catch {
+                print("Error formattedQuestion \(error)")
+                return ""
+            }
+        }
+        var answers: [Answer] {
+            do {
+              let correct = [Answer(text: try AttributedString(markdown: correctAnswer), isCorrect: true)]
+              let incorrects = try incorrectAnswers.map { answer in
+                    Answer(text: try AttributedString(markdown: answer), isCorrect: false)
+                }
+                
+                let allAnswers = correct + incorrects
+                return allAnswers.shuffled()
+            } catch {
+                print("Error setting answers \(error)")
+                return []
+            }
+        }
+    }
+}
