20b9e781f39f4e1514dd35c7c7b30bd0d823a25b
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-02 13:07:14 +0500

    Design EndView

--- FlashCard Quiz/FlashCard Quiz/Views/FlashView.swift
+++ FlashCard Quiz/FlashCard Quiz/Views/FlashView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct FlashView: View {
+    
+    @EnvironmentObject var triviaManager: TriviaManager
+    
     var body: some View {
-        QuestionView()
+        if triviaManager.reachedEnd {
+            VStack(spacing: 20) {
+                Text("FlashCard Quiz")
+                    .lilacTitle()
+                Text("Congratulation! You completed your Task-1🥳")
+                
+                Text("You scored \(triviaManager.score) out of \(triviaManager.length)")
+                
+                Button {
+                    Task.init {
+                        await triviaManager.fetchTrivia()
+                    }
+                } label: {
+                    LetGoButton(text: "Play Again")
+                }
+
+            }
+            .foregroundColor(Color("AccentColor"))
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+        } else {
+            QuestionView()
+                .environmentObject(triviaManager)
+        }
     }
 }
 
 struct FlashView_Previews: PreviewProvider {
     static var previews: some View {
         FlashView()
+            .environmentObject(TriviaManager())
     }
 }
